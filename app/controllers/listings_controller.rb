b86feea1d58ff1fa8aaab212e4e8f16db54a3837
Sky12072 <12072@coderacademy.edu.au>
2022-06-22 13:18:20 +1000

    add relationship from user to listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -23,6 +23,7 @@ class ListingsController < ApplicationController
   # POST /listings or /listings.json
   def create
     @listing = Listing.new(listing_params)
+    @listing.user = current_user
 
     respond_to do |format|
       if @listing.save
@@ -71,5 +72,5 @@ class ListingsController < ApplicationController
 
     def set_form_vars
       @categories = Category.all
-    end
+    end 
 end

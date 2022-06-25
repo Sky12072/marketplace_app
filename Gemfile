06ee6a9a9bc5b5026b40d4a49e18540ecebb2690
Sky12072 <12072@coderacademy.edu.au>
2022-06-25 13:03:23 +1000

    added messaging features

--- Gemfile
+++ Gemfile
@@ -50,3 +50,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "devise", "~> 4.8"
+
+gem 'kramdown' 
+
+gem 'kramdown-parser-gfm'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
+require 'kramdown'
+require 'kramdown-parser-gfm' 
+
 module ApplicationHelper
+
+    def gravatar_for(user, options = { size: 200})
+     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+     size = options[:size]
+     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+     image_tag("user.png", alt: user.username, class: "border-radius-50", size: 50)
+    end
+ 
+    def markdown_to_html(text)
+        Kramdown::Document.new(text, input: "GFM").to_html
+    end
+ 
+    def listing_author(listing)
+        user_signed_in? && current_user.id == listing.user_id
+    end
+ 
 end

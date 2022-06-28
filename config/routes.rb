fa829ec18f3ad3af656e3f993579a034f7d317c6
Sky12072 <12072@coderacademy.edu.au>
2022-06-28 13:02:28 +1000

    bootstrap design for homepage added

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,15 @@ Rails.application.routes.draw do
   resources :listings
   
 
-  resources :conversations do
-    resources :messages
-  end
+ 
 
 
   devise_for :users
   root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  resources :conversations do
+    resources :messages
+  end
   post "listings/:id/message", to: "messages#create", as: "place_interest"
 
   get '/about' => 'pages#about', as: "about"

fd34fec3f8a1023b126920e3c812be96936dadcf
Sky12072 <12072@coderacademy.edu.au>
2022-07-05 18:02:59 +1000

    revert and fix gravatar_for method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,14 @@ require 'kramdown'
 require 'kramdown-parser-gfm' 
 
 module ApplicationHelper
+
+    def gravatar_for(user, options = { size: 200})
+        if user.avatar.attached? 
+            image_tag(user.avatar, size: 40, class: "rounded-circle")
+        else
+            image_tag("user.png", alt: user.username, class: "rounded-circle", size: 40)
+        end
+    end
  
     def markdown_to_html(text)
         Kramdown::Document.new(text, input: "GFM").to_html
@@ -10,15 +18,6 @@ module ApplicationHelper
     def listing_author(listing)
         user_signed_in? && current_user.id == listing.user_id
     end
-
-    def user_avatar(user, size=40)
-        if user.avatar.attached? 
-            image_tag(user.avatar, size: 40, class: "rounded-circle")
-        else
-            image_tag("user.png", alt: user.username, class: "rounded-circle", size: 40)
-        end
-    end
-
     
  
 end

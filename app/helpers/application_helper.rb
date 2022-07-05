80e76673c10113a8f5538f62303b45a19e0723c5
Sky12072 <12072@coderacademy.edu.au>
2022-07-05 17:12:39 +1000

    sender avatar pic is fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,6 @@ require 'kramdown'
 require 'kramdown-parser-gfm' 
 
 module ApplicationHelper
-
-    def gravatar_for(user, options = { size: 200})
-     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-     size = options[:size]
-     image_tag("user.png", alt: user.username, class: "border-radius-50", size: 40)
-    end
  
     def markdown_to_html(text)
         Kramdown::Document.new(text, input: "GFM").to_html
@@ -19,7 +13,7 @@ module ApplicationHelper
 
     def user_avatar(user, size=40)
         if user.avatar.attached? 
-            user.avatar.variant(resize: "40x40!", class: "rounded-circle")
+            image_tag(user.avatar, size: 40, class: "rounded-circle")
         else
             image_tag("user.png", alt: user.username, class: "rounded-circle", size: 40)
         end

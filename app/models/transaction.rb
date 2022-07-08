4b3034d4b3dd6d23e3ad0f19bcc9b3ff8068389e
Sky12072 <12072@coderacademy.edu.au>
2022-07-08 10:36:00 +1000

    fixed the heroku migration issue

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,3 +3,6 @@ class Transaction < ApplicationRecord
   belongs_to :coder, foreign_key: "coder_id", class_name: "User"
   belongs_to :client, foreign_key: "client_id", class_name: "User"
 end
+
+# Please read !
+# please ignore this model as for now, because I'm making this for future agile development, not for the assessment. thanks!
--- db/migrate/20220622044442_fix_column_name.rb
+++
@@ -1,5 +0,0 @@
-class FixColumnName < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :listings, :closed, :closed
-  end
-end
